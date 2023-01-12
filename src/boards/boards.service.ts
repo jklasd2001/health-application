c67aafdd8e06c40d19fba8450f1994328def3602
jklasd2001 <jklasd2001@naver.com>
2023-01-12 20:04:02 +0900

    feat: Board 모델 추가

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common'
+
+import { Board } from './boards.model'
+
+@Injectable()
+export class BoardsService {
+  private boards: Board[] = []
+
+  getAllBoards(): Board[] {
+    return this.boards
+  }
+}
