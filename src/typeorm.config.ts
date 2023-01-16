350bcdc7393bbb3c18f781fdbea165ddd5f38499
김영수 주임 연구원 <jklasd2001@musma.net>
2023-01-16 23:16:43 +0900

    feat: TypeORM 환경 설정

---
+++ src/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '0000',
+  database: 'HealthProject',
+  entities: [__dirname + '/../**/*.entity.{ts}'],
+  /**
+   * true 값을 주면 애플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이, 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해줍니다.
+   */
+  synchronize: true,
+}
